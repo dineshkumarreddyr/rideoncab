ad9ba9e3d92b0560bcb463ce1afa94d87999f707
Dinesh Kumar <dinesh.rachumalla@hotmail.com>
2015-07-31 23:29:30 +0530

    Date picker changes

--- app/helpers/common.js
+++ app/helpers/common.js
@@ -5,7 +5,24 @@
 		this.setHours(this.getHours()+h);
 		return this;
 	}
-	var fnMinutes = function(){
-		return (new Date().addHours(2));
-	}
+	Date.prototype.daysBetween = function( date1, date2 ) {
+  //Get 1 day in milliseconds
+  var one_day=1000*60*60*24;
+
+  // Convert both dates to milliseconds
+  var date1_ms = date1.getTime();
+  var date2_ms = date2.getTime();
+
+  // Calculate the difference in milliseconds
+  var difference_ms = date2_ms - date1_ms;
+  
+  // Convert back to days and return
+  return Math.round(difference_ms/one_day); 
+}
+var generatepicker = function(c,x){
+	debugger;
+}
+var fnMinutes = function(){
+	return (new Date().addHours(2));
+}
 })();
